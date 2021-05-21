60eca70adad7a0cb4926fbb1e066cea214397e77
esales <ericsales0@gmail.com>
2021-05-21 01:26:32 -0300

    commit inicial

---
+++ Aula08/pagina2.php
@@ -0,0 +1,13 @@
+<?php   
+
+$nome = $_REQUEST['nome'];
+$area = $_REQUEST['area'];
+$modalidade = $_REQUEST['modalidade'];
+
+echo 'Nome: ' . $nome;
+echo '<br><br>';
+echo 'Área: ' . $area;
+echo '<br><br>';
+echo 'Modalidade: ' . $modalidade;
+
+?>
